77aefc328ad6f55e2e4f2d1779e566f7ea3de0d8
serhiyben <serhiyben2@gmail.com>
2025-12-14 17:54:02 +0200

    fix: fix warning on app

--- app/products/init.py
+++ app/products/init.py
@@ -1,2 +1,12 @@
-from app.products.views import products_bp
-app.register_blueprint(products_bp, url_prefix="/products")
+from flask import Flask
+from typing import Optional
+
+app: Optional[Flask]
+
+def create_app():
+    global app
+    app = Flask(__name__)
+    ...
+    return app
+
+app = create_app()
