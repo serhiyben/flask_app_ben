907ba5f49e06f2603835d1a65e9f1ae8b8dd2c2a
serhiyben <serhiyben2@gmail.com>
2025-12-14 18:56:57 +0200

    feat:add notifications with secret key

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,29 @@
 from flask import Flask
 
+
 def create_app():
-    # створюємо Flask-додаток
+
     app = Flask(__name__)
 
-    # завантажуємо конфігурацію (якщо є config.py)
     app.config.from_pyfile("../config.py", silent=True)
 
-    # Імпортуємо та реєструємо Blueprints
     from app.users.views import users_bp
+
     app.register_blueprint(users_bp, url_prefix="/users")
 
     from app.pages.views import pages_bp
+
     app.register_blueprint(pages_bp)
 
-    # Якщо є другий Blueprint (products)
     try:
         from app.products.views import products_bp
+
         app.register_blueprint(products_bp, url_prefix="/products")
     except ModuleNotFoundError:
-        # Якщо products ще не створений — просто пропускаємо
+
         pass
 
     return app
 
-# створюємо головний екземпляр застосунку
+
 app = create_app()
---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'my_secret_key_for_lab4'
+
