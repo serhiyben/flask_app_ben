8e588ff6c2920efcf04c67b5f4284a461675afe9
serhiyben <serhiyben2@gmail.com>
2025-12-14 23:17:36 +0200

    feat: add created_at field

--- app/products/models.py
+++ app/products/models.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Integer, String, Float, ForeignKey, Boolean
+from sqlalchemy import Integer, String, Float, ForeignKey, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql import func
 from app import db
 from typing import List, Optional
+from datetime import datetime
 
 class Category(db.Model):
     __tablename__ = "categories"
@@ -9,30 +11,31 @@ class Category(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(128), nullable=False, unique=True)
 
-    
     products: Mapped[List["Product"]] = relationship(
-        "Product", 
-        back_populates="category",
-        lazy="select"
+        "Product", back_populates="category", lazy="select"
     )
 
     def __repr__(self):
         return f"<Category {self.name}>"
 
+
 class Product(db.Model):
-    __tablename__ = 'products'
+    __tablename__ = "products"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     price: Mapped[float] = mapped_column(Float, nullable=False)
     
-   
     
     active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
     
     
-    category_id: Mapped[Optional[int]] = mapped_column(ForeignKey('categories.id'))
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
+
     
+    category_id: Mapped[Optional[int]] = mapped_column(ForeignKey("categories.id"))
     category: Mapped["Category"] = relationship("Category", back_populates="products")
 
     def __repr__(self):
